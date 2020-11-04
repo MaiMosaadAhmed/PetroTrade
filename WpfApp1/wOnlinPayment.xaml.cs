7a3662ccb5aac1d0e506b500ee2da098a03b551c
Mai Ahmed <Mai.M.Ahmed@outlook.com>
2020-11-04 12:31:57 +0200

    initial commit

---
+++ WpfApp1/wOnlinPayment.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for wOnlinPayment.xaml
+    /// </summary>
+    public partial class wOnlinPayment : Window
+    {
+        public wOnlinPayment()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("تم السداد بنجاح", "بتروتريد", MessageBoxButton.OK, MessageBoxImage.Information);
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+    }
+}
