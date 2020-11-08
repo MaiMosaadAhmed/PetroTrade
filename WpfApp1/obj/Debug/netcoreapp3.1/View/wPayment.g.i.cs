7e47a1a1805402c23ba24c7a5a46b21cfbea9e4b
Mai Ahmed <Mai.M.Ahmed@outlook.com>
2020-11-08 11:37:02 +0200

    test

--- WpfApp1/obj/Debug/netcoreapp3.1/View/wPayment.g.i.cs
+++ WpfApp1/obj/Debug/netcoreapp3.1/View/wPayment.g.i.cs
@@ -213,7 +213,7 @@ namespace WpfApp1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp1;component/view/wpayment.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp1;V1.0.0.0;component/view/wpayment.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\wPayment.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
